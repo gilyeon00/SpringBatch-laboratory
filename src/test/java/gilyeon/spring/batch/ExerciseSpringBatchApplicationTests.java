a93e3d0e6d9d7944d2605717b5f45537bc12bea4
gilyeon00 <12venus08@naver.com>
2023-06-23 14:23:18 +0900

    Spring Batch 실습을 위한 프로젝트 세팅

---
+++ src/test/java/gilyeon/spring/batch/ExerciseSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package gilyeon.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
