a8535ce7c23a526d54798296a070ec26f224ba56
gilyeon00 <12venus08@naver.com>
2023-10-20 12:59:29 +0900

    Update FlowJob

--- src/main/java/gilyeon/spring/batch/FlowJobConfiguration.java
+++ src/main/java/gilyeon/spring/batch/FlowJobConfiguration.java
@@ -6,15 +6,12 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.Flow;
 
 @Configuration
 @RequiredArgsConstructor
@@ -23,81 +20,58 @@ public class FlowJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-
-//    step1 이 성공하면 step2 실행
-//    step1 이 실패하면 step3 실행
     @Bean
-    public Job batchJob() {
-        return jobBuilderFactory.get("batchJob")
+    public Job myjob(){
+        return jobBuilderFactory.get("myjob")
                 .start(step1())
-                .on("COMPLETED")
-                .to(step2())
+                    .on("FAILED")
+                    .to(step2())
                 .from(step1())
-                .on("FAILED")
-                .to(step3())
+                    .on("*")
+                    .to(step3())
                 .end()
-                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Job batchFlows() {
-        return jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .on("COMPLETED")
-                .to(step2())
-                .from(step1())
-                .on("FAILED").to(step3())
-                .end()
-                .incrementer(new RunIdIncrementer())
-                .build();
-    }
-
-    @Bean
-    public Flow flowA() {
-        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
-        flowBuilder.start(step1())
-                .next(step2())
-                .end();
-        return flowBuilder.build();
-    }
-
-    @Bean
-    public Step step1() {
+    public Step step1(){
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("==============step1 has executed================");
+                        System.out.println("==============step1 executed=============");
+                        FAIL
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                })
+                .build();
     }
 
-
     @Bean
-    public Step step2() {
+    public Step step2(){
         return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("==============step2 has executed================");
+                        System.out.println("==============step2 executed=============");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                })
+                .build();
     }
 
-
-
     @Bean
-    public Step step3() {
+    public Step step3(){
         return stepBuilderFactory.get("step3")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("==============step3 has executed================");
+                        System.out.println("==============step3 executed=============");
                         return RepeatStatus.FINISHED;
                     }
-                }).build();
+                })
+                .build();
     }
+
+
 }
