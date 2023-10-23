e8bc24bf434717ab6e76b52e7ce3a660734536fd
gilyeon00 <12venus08@naver.com>
2023-10-23 13:42:46 +0900

    CREATE custom JobExecutionDecider

--- src/main/java/gilyeon/spring/batch/FlowJobConfiguration.java
+++ src/main/java/gilyeon/spring/batch/FlowJobConfiguration.java
@@ -1,12 +1,12 @@
 package gilyeon.spring.batch;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -22,17 +22,55 @@ public class FlowJobConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job myjob(){
-        return jobBuilderFactory.get("myjob2")
+    public Job deciderJob(){
+        return jobBuilderFactory.get("deciderJob")
                 .start(step1())
-                    .on("FAILED")
-                    .to(step2())
-                .from(step1())
-                    .on("*")
-                    .to(step3())
+                .next(decider())
+                .from(decider()).on("ODD").to(oddStep())
+                .from(decider()).on("EVEN").to(evenStep())
                 .end()
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
+
+    @Bean
+    public JobExecutionDecider decider() {
+        return new OddDecider();
+    }
+
+    public static class OddDecider implements JobExecutionDecider{
+        private int count = 0;
+        @Override
+        public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+            count ++;
+            if(count % 2 == 0){
+                return new FlowExecutionStatus("EVEN");
+            } else {
+                return new FlowExecutionStatus("ODD");
+            }
+        }
+    }
+
+    @Bean
+    public Step evenStep(){
+        return stepBuilderFactory.get("evenStep")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("========evenStep executed==========");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step oddStep(){
+        return stepBuilderFactory.get("oddStep")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println("========oddStep executed==========");
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
 
     @Bean
     public Step step1(){
@@ -41,33 +79,6 @@ public class FlowJobConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("==============step1 executed=============");
-                        contribution.setExitStatus(ExitStatus.FAILED);
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step2(){
-        return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("==============step2 executed=============");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step3(){
-        return stepBuilderFactory.get("step3")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("==============step3 executed=============");
                         return RepeatStatus.FINISHED;
                     }
                 })
