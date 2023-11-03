853ce2aae0e688e02c26a041086541b2f38468ea
gilyeon00 <12venus08@naver.com>
2023-11-03 10:31:37 +0900

    사용자 정의 ExitStatus example

--- src/main/java/gilyeon/spring/batch/JobConfiguration.java
+++ src/main/java/gilyeon/spring/batch/JobConfiguration.java
@@ -1,90 +1,63 @@
-//package gilyeon.spring.batch;
-//
-//import gilyeon.spring.batch.entity.Member;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.batch.core.*;
-//import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-//import org.springframework.batch.core.launch.JobLauncher;
-//import org.springframework.batch.core.launch.support.RunIdIncrementer;
-//import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
-//import org.springframework.batch.repeat.RepeatStatus;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.List;
-//
-//
-//@Configuration
-//@RequiredArgsConstructor
-//public class JobConfiguration {
-//
-//    private final JobBuilderFactory jobBuilderFactory;
-//    private final StepBuilderFactory stepBuilderFactory;
-//
-//
-//    @Bean
-//    public Job parentJob() {
-//        return jobBuilderFactory.get("parentJob")
-//                .start(jobStep(null))
-//                .next(step2())
-//                .incrementer(new RunIdIncrementer())
-//                .build();
-//    }
-//
-//    // ExecutionContext 에 있는 값을 key 를 통해 가져올 수 있음
-//    private DefaultJobParametersExtractor myJobParametersExtractor() {
-//        DefaultJobParametersExtractor extractor = new DefaultJobParametersExtractor();
-//        extractor.setKeys(new String[]{"name"});
-//        return extractor;
-//    }
-//    @Bean
-//    public Step jobStep (JobLauncher jobLauncher) {
-//        return stepBuilderFactory.get("my-jobStep")
-//                .job(childJob())
-//                .launcher(jobLauncher)
-////                .parametersExtractor(new JobParametersExtractor() {
-////                    @Override
-////                    public JobParameters getJobParameters(Job job, StepExecution stepExecution) {
-////                        return null;
-////                    }
-////                })
-//                .parametersExtractor(myJobParametersExtractor())
-//                .listener(new StepExecutionListener() {
-//                    @Override
-//                    public void beforeStep(StepExecution stepExecution) {
-//                        stepExecution.getExecutionContext().putString("name", "user1");
-//                    }
-//
-//                    @Override
-//                    public ExitStatus afterStep(StepExecution stepExecution) {
-//                        return null;
-//                    }
-//                })
-//                .build();
-//    }
-//
-//    @Bean
-//    public Job childJob() {
-//        return jobBuilderFactory.get("childJob")
-//                .start(step1())
-//                .incrementer(new CustomJobParametersIncrementer())
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step step1 () {
-//        return stepBuilderFactory.get("step1")
-//                .tasklet(((contribution, chunkContext) -> RepeatStatus.FINISHED))
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step step2 () {
-//        return stepBuilderFactory.get("step2")
-//                .tasklet(((contribution, chunkContext) -> RepeatStatus.FINISHED))
-//                .build();
-//    }
-//
-//
-//}
+package gilyeon.spring.batch;
+
+import gilyeon.spring.batch.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.job.DefaultJobParametersExtractor;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job exitStatusExampleJob() {
+        return jobBuilderFactory.get("exitStatusExampleJob")
+                .start(step1())
+                    .on("FAILED")
+                    .to(step2())
+                    .on("PASS")
+                    .stop()
+                .end()
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println(">>>> step1 executed");
+                    contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                }))
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(((contribution, chunkContext) -> {
+                    System.out.println(">>>> step2 executed");
+                    return  RepeatStatus.FINISHED;
+                }))
+                .listener(new PassCheckListener())
+                .build();
+    }
+
+
+}
